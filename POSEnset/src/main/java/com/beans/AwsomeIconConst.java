5916e1d69142bab6caca1ee5735427ad42003697
elmottaki <elmottaki@cffa68dc-4786-425a-813c-4794a14e1071>
2015-12-03 02:13:35 +0000

    some modification

---
+++ POSEnset/src/main/java/com/beans/AwsomeIconConst.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.beans;
+
+/**
+ *
+ * @author elmottaki
+ */
+public class AwsomeIconConst {
+    public static char ADD_ICON = '\uf0c5';
+    public static char MOD_ICON = '\uf0ad';
+    public static char DEL_ICON = '\uf014';
+    public static char NEW_ICON = '\uf0fe';
+    public static char SEARCH_PLUS_ICON= '\uf00e';
+    public static char VALIDE_ICON = '\uf046';
+    public static char CANCEL_ICON = '\uf05e';
+}

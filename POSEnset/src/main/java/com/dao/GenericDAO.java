44b024740b897674dbf25aaeb6f202b14c1b9e7a
issamkhalil11 <issamkhalil11@cffa68dc-4786-425a-813c-4794a14e1071>
2015-12-30 09:17:13 +0000

    issam

--- POSEnset/src/main/java/com/dao/GenericDAO.java
+++
@@ -1,9 +0,0 @@
-package com.dao;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
-public abstract class GenericDAO {
-	@PersistenceContext
-	protected EntityManager em;
-}
